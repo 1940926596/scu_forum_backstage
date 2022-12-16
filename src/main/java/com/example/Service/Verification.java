f7f135d04440dcfbf8becef4973fb12b0d5c9593
Aerial <1940926596@qq.com>
2022-12-16 13:37:39 +0800

    v-1.1.6

--- src/main/java/com/example/Service/Verification.java
+++ src/main/java/com/example/Service/Verification.java
@@ -9,7 +9,7 @@ public class Verification {
      public static String postVerification(String code,String name){
         try{
             //api url地址
-            String url = "http://43.143.211.83:8084/addCodeList?code="+code+"&name="+name;
+            String url = "http://localhost:8080/addCodeList?code="+code+"&name="+name;
             // 封装参数，千万不要替换为Map与HashMap，否则参数无法传递
             MultiValueMap<String, String> params= new LinkedMultiValueMap<String, String>();
             params.add("name", name);
